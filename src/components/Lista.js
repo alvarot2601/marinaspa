9b15b9d9f3ad4044c93c36d2e09e0c09fb6c4793
alvarot2601 <alvarot2601@gmail.com>
2023-09-21 14:56:58 +0200

    añado algunos keys en listas

--- src/components/Lista.js
+++ src/components/Lista.js
@@ -9,7 +9,7 @@ const Lista = ({ obj }) => {
   return obj.map((singleObj, i) => {
     const reverseRowClass = i % 2 === 0 ? "" : "order-1";
     return (
-      <div className="flex text-zinc-700">
+      <div key={`lista-${i}`} className="flex text-zinc-700">
         <div
           className={`w-full md:w-7/12 lg:w-1/2 px-7 py-5 ${reverseRowClass} flex flex-col justify-center`}
         >
@@ -79,9 +79,9 @@ const Lista = ({ obj }) => {
                             <p>{treatment[1]}</p>
                             {
                               
-                              treatment[3].map((program) => {
+                              treatment[3].map((program, index2) => {
                                 return (
-                                  <div className="flex justify-between">
+                                  <div key={`program-${index2}`} className="flex justify-between">
                                     <span>{program[0]}</span>
                                     <span>{program[1]}€</span>
                                   </div>
