0bc6b9c168d92192180c9052db04d640ec789440
alvarot2601 <alvarot2601@gmail.com>
2023-10-20 17:27:24 +0200

    lazy imports

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { React, lazy, Suspense } from 'react';
 import logo from './logo.svg';
 import './styles/css/styles.css';
 
@@ -6,34 +7,49 @@ import Home from './pages/Home';
 import Tratamientos from './pages/Tratamientos';
 import ProgramasFacialesPage from './pages/ProgramasFaciales';
 
-import { useRef } from 'react';
-import EsteticaGeneralPage from './pages/EsteticaGeneral';
-import SiluetaCorporalPage from './pages/SiluetaCorporal';
-import BienestarRelajacionPage from './pages/BienestarRelajacion';
-import DepilacionPage from './pages/DepilacionFotodepilacion';
-import Contacto from './pages/Contacto';
-import Galeria from './pages/Galeria';
-import PoliticaCookies from './pages/Politica-cookies';
-import PoliticaPrivacidad from './pages/PoliticaPrivacidad';
-import AvisoLegal from './pages/Aviso-legal';
+
+
+
+
+//import SiluetaCorporalPage from './pages/SiluetaCorporal';
+//import BienestarRelajacionPage from './pages/BienestarRelajacion';
+//import DepilacionPage from './pages/DepilacionFotodepilacion';
+//import Contacto from './pages/Contacto';
+//import Galeria from './pages/Galeria';
+//import PoliticaCookies from './pages/Politica-cookies';
+//import PoliticaPrivacidad from './pages/PoliticaPrivacidad';
+//import AvisoLegal from './pages/Aviso-legal';
+const EsteticaGeneralPage = lazy(()=>import("./pages/EsteticaGeneral"));
+
+const SiluetaCorporalPage = lazy(()=>import("./pages/SiluetaCorporal"));
+const BienestarRelajacionPage = lazy(()=>import("./pages/BienestarRelajacion"));
+const DepilacionPage = lazy(()=>import("./pages/DepilacionFotodepilacion"));
+const Contacto = lazy(()=>import("./pages/Contacto"));
+const Galeria = lazy(()=>import("./pages/Galeria"));
+const PoliticaCookies = lazy(()=>import("./pages/Politica-cookies"));
+const PoliticaPrivacidad = lazy(()=>import("./pages/PoliticaPrivacidad"));
+const AvisoLegal = lazy(()=>import("./pages/Aviso-legal"));
+
+
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/tratamientos" element={<Tratamientos/>}></Route>
-        <Route path="/programas-faciales" element={<ProgramasFacialesPage title="Programas Faciales"/>} ></Route>
-        <Route path="/estetica-general" element={<EsteticaGeneralPage title="Estética General" />} ></Route>
-        <Route path="/silueta-corporal" element={<SiluetaCorporalPage/>} ></Route>
-        <Route path="/bienestar-y-relajacion" element={<BienestarRelajacionPage/>} ></Route>
-        <Route path="/depilacion-y-fotodepilacion" element={<DepilacionPage/>} ></Route>
-        <Route path="/galeria" element={<Galeria/>} ></Route>
-        <Route path="/contacto" element={<Contacto/>} ></Route>
-        <Route path="/politica-cookies" element={<PoliticaCookies/>}></Route>
-        <Route path="/politica-privacidad" element={<PoliticaPrivacidad/>}></Route>
-        <Route path="/aviso-legal" element={<AvisoLegal/>}></Route>
-      </Routes>
-
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/tratamientos" element={<Tratamientos />}></Route>
+          <Route path="/programas-faciales" element={<ProgramasFacialesPage title="Programas Faciales" />} ></Route>
+          <Route path="/estetica-general" element={<EsteticaGeneralPage title="Estética General" />} ></Route>
+          <Route path="/silueta-corporal" element={<SiluetaCorporalPage />} ></Route>
+          <Route path="/bienestar-y-relajacion" element={<BienestarRelajacionPage />} ></Route>
+          <Route path="/depilacion-y-fotodepilacion" element={<DepilacionPage />} ></Route>
+          <Route path="/galeria" element={<Galeria />} ></Route>
+          <Route path="/contacto" element={<Contacto />} ></Route>
+          <Route path="/politica-cookies" element={<PoliticaCookies />}></Route>
+          <Route path="/politica-privacidad" element={<PoliticaPrivacidad />}></Route>
+          <Route path="/aviso-legal" element={<AvisoLegal />}></Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
